2eb2949999b1e016c734ec59bb370c8fab805f13
adferrin <adferrin@icloud.com>
2021-04-07 15:04:23 -0700

    all routes passing tests

--- routes/api.js
+++ routes/api.js
@@ -62,21 +62,59 @@ module.exports = function (app) {
 
 
 
-  app.route('/api/books/:id')
+  app
+    .route('/api/books/:id')
     .get(function (req, res){
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      Book.findById(bookid, (err, data) => {
+        if(!data) {
+          res.send("no book exists");
+        } else {
+          res.json({
+            comments: data.comments,
+            _id: data._id,
+            title: data.title,
+            commentcount: data.comments.length,
+          });
+        }
+      });
     })
     
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if(!comment) {
+        res.send("missing required field comment");
+        return;
+      }
+      Book.findById(bookid, (err, bookdata) => {
+        if(!bookdata) {
+          res.send("no book exists");
+        } else {
+          bookdata.comments.push(comment);
+          bookdata.save((err, saveData) => {
+            res.json({
+              comments: saveData.comments,
+              _id: saveData._id,
+              title: saveData.title,
+              commentcount: saveData.comments.length,
+            });
+          });
+        }
+      });
     })
     
     .delete(function(req, res){
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
-    });
-  
+      Book.findByIdAndRemove(bookid, (err, data) => {
+        if (err || !data) {
+          res.send("no book exists");
+        } else {
+          res.send("delete successful");
+        }
+      });
+    }); 
 };
